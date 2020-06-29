cf9fce59ac075adb65985aee9d935789d847ec33
gyetol <econostein@naver.com>
2020-06-29 16:34:10 +0900

    file removing & moving

--- filesystem/testmain.c
+++ filesystem/testmain.c
@@ -12,6 +12,10 @@ int main(){
 //    cmd_rmdir("test", &msg);
 //    printf("%s",msg);
     cmd_fileCp("fileDelete.c", "test.c", &msg);
+	printf("%s",msg);
+	cmd_fileRm("fileDelete.c",&msg);
     printf("%s",msg);
+	cmd_fileMv("test.c","aaaa.c",&msg);
+	printf("%s",msg);
     return 0;
 }
