819c0685d6811832b36055e1210caf0b8243e2fe
tlfzgjadyd <tlfzgjadyd@naver.com>
2020-06-28 19:06:51 -0700

    fileDanzi start

---
+++ test.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+int test(){
+while(1){
+	buf[BUFSIZ];
+	fgets(0, buf, sizeof(buf);
+	if(access(buf, 00)==-1)
+		printf("no such file or directory");
+		return -1;
+	}
+	if(isFileOrDir(dir)==1)
+	{
+		printf("this is file");
+		//exec(다운로드 요청 혹은 파일 열기 요청)
+	}
+	else if(isFileOrDir(dir)==0)
+	{
+		printf("this is directory");
+		//exec(일단 cd 요청부터 받기)
+	}
+	else if(isFileOrDir(dir)==-1)
+		printf("no such file or directory"); 
+}
+
