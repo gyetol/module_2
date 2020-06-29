b68336e57739ef5f337dcaa1bcde4b5204dddd28
moosong <songe08@gmail.com>
2020-06-29 01:11:03 -0700

    file copy done

---
+++ Client/console.c
@@ -0,0 +1,132 @@
+//
+// Created by linux on 6/28/20.
+//
+
+#include "console.h"
+#include <ncurses.h>
+#define KEYBOARD_UP 65
+#define KEYBOARD_DOWN 66
+#define KEYBOARD_LEFT 68
+#define KEYBOARD_RIGHT 67
+#define KEYBOARD_ENTER 13
+#define KEYBOARD_BACKSPACE 127
+
+#define MENU_MAIN 0
+#define MENU_CONNECT 1
+#define MENU_HISTORY 2
+#define MENU_EXIT 3
+#define MENU_SIGNIN 4
+#define MENU_SIGNOUT 5
+#define MENU_PLAY 6
+#define MENU_INGAME 7
+#define MENU_STATISTICS 8
+#define MENU_2PLOGIN 9
+#define MENU_2PMAIN 10
+
+#define MENU_GAMESTART 21
+#define MENU_NEXTTURN 22
+#define MENU_PLAYER1 23
+#define MENU_PLAYER2 24
+
+
+#define MENU_ID 100
+#define MENU_PASS 101
+
+#define MENU_WIN 200
+#define MENU_LOSE 201
+
+#define IDPASSLENGTH 15
+void init_scr()
+{
+    initscr();
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_MAGENTA);
+    init_pair(2, COLOR_MAGENTA, COLOR_WHITE);
+    init_pair(3, COLOR_WHITE, COLOR_MAGENTA);
+}
+void init_updownWindow(WINDOW *upWindow, WINDOW *middleWindow, WINDOW *downWindow)
+{
+    upWindow = subwin(stdscr, 14, 80, 0, 0);
+    wbkgd(upWindow, COLOR_PAIR(1));
+
+    middleWindow = subwin(stdscr, 7, 80, 14, 0);
+    wbkgd(middleWindow, COLOR_PAIR(2));
+
+    downWindow = subwin(stdscr, 3, 80, 21, 0);
+    wbkgd(downWindow, COLOR_PAIR(3));
+    attron(COLOR_PAIR(1));
+}
+int _mainMenu(){
+    werase(stdscr); // Clear Window
+    curs_set(0); // Not need Cursor pointer
+    char key; // User Input
+    int selectingMenu = MENU_CONNECT;
+
+    WINDOW *upMenu, *middleWindow,*downMenu;
+    init_updownWindow(upMenu, middleWindow, downMenu);
+
+    mvprintw(3, 34, "FILE DANZI");
+    mvprintw(7, 32, "cute FTP Cleint");
+    attron(COLOR_PAIR(2));
+    mvprintw(16, 27, "INSERT SERVER'S IP ADDRESS");
+    mvprintw(18, 27, "IP ADDRESS : ");
+
+    while(1){
+        attron(COLOR_PAIR(1));
+        mvprintw(22, 15, "CONNECT IP");
+        mvprintw(22, 36, "HISTORY");
+        mvprintw(22, 58, "EXIT");
+
+        attron(A_STANDOUT | A_UNDERLINE); // selected effect
+
+        // Overwriting menu font
+        switch(selectingMenu){
+            case MENU_CONNECT :
+                mvprintw(22, 15, "CONNECT IP");
+                break;
+            case MENU_HISTORY :
+                mvprintw(22, 36, "HISTORY");
+                break;
+            case MENU_EXIT :
+                mvprintw(22, 58, "EXIT");
+                break;
+        }
+
+        attroff(A_STANDOUT | A_UNDERLINE);
+
+        refresh();
+
+        key = getchar();
+
+        switch(key){
+            case KEYBOARD_RIGHT :
+                if(selectingMenu == MENU_CONNECT)
+                    selectingMenu = MENU_HISTORY;
+                else if(selectingMenu == MENU_HISTORY)
+                    selectingMenu = MENU_EXIT;
+                else if(selectingMenu == MENU_EXIT)
+                    selectingMenu = MENU_CONNECT;
+                break;
+            case KEYBOARD_LEFT :
+                if(selectingMenu == MENU_HISTORY)
+                    selectingMenu = MENU_CONNECT;
+                else if(selectingMenu == MENU_EXIT)
+                    selectingMenu = MENU_HISTORY;
+                else if(selectingMenu == MENU_CONNECT)
+                    selectingMenu = MENU_EXIT;
+                break;
+            case KEYBOARD_ENTER :
+                delwin(upMenu);
+                delwin(middleWindow);
+                delwin(downMenu);
+                return selectingMenu;
+        }
+    }
+}
+
+int main(){
+    init_scr();
+    _mainMenu();
+    endwin();
+    return 0;
+}
---
+++ Client/console.h
@@ -0,0 +1,8 @@
+//
+// Created by linux on 6/28/20.
+//
+
+#ifndef MODULE_2_CONSOLE_H
+#define MODULE_2_CONSOLE_H
+
+#endif //MODULE_2_CONSOLE_H
