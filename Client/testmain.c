ab95fec3c8787f763f2f0e824361957783646d43
moosong <songe08@gmail.com>
2020-06-30 11:39:43 -0700

    update moosong

---
+++ Client/testmain.c
@@ -0,0 +1,13 @@
+//
+// Created by linux on 6/30/20.
+//
+//#include "dirmanager.h"
+//#include <stdio.h>
+//#include <dirent.h>
+#include <sys/stat.h>
+
+int main(){
+    mkdir("./test", 0755);
+
+    return 0;
+}
