20f5d5326660d891b74866c89e1acf247d257fb3
moon-hw <hyowon970130@naver.com>
2020-07-02 05:23:31 -0700

    hyowon

--- sendRequest.c
+++ sendRequest.c
@@ -8,13 +8,12 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "FtpCommand.h"
-#include "ClientSocket.h"
-#define BUFSIZ 1024
+#include "listOpen.h"
+#define END_OF_PROTOCOL "\r\n"
 
-int listDownload(char *type, char *path, char *ip int sSock){
+int listDownload(char *type, char *path, char *ip, int cSock){
 
-	if (type == NULL || path == NULL || ip == NULL){
+   if (type == NULL || path == NULL || ip == NULL){
 		perror("listDownload");
 		return -1;
 	}
@@ -35,17 +34,6 @@ int listDownload(char *type, char *path, char *ip int sSock){
 	strcat(buf,"192.168.198.141");
     
 	while(1){
-	int nRead = read(fd, buf, sizeof(buf));
-
-	if (nRead < 0){
-		perror("nRead");
-		close(fd);
-		return -1;
-	}
-
-	else if(nRead == 0){
-		break;
-	}
 
 	int nWritten=write(cSock,buf,sizeof(buf));
 	if(nWritten<0){
@@ -55,10 +43,11 @@ int listDownload(char *type, char *path, char *ip int sSock){
 	   } 
 	}
 	close(fd);
+	listOpen();
 
 }
 
-int fileDownload(char *type, char *path, char *ip, int sSock){
+int fileDownload(char *type, char *path, char *ip, int cSock){
 
 	int readBytes, totalBytes;
 
@@ -67,9 +56,9 @@ int fileDownload(char *type, char *path, char *ip, int sSock){
 		return -1;
 	}
 
-	int fd = open("./requst.txt", O_RDWR, O_CREAT, O_TRUNC, 0744);	
+	int fd = open("./request.txt", O_RDWR, O_CREAT, O_TRUNC, 0744);	
 		//mode는 0700 파일 소유자에게 읽기 쓰기, 쓰기 실행 권한 
-		//00040으로 그룹에게 읽기 권한
+		//0004 0으로 그룹에게 읽기 권한
 		//00004으로 기타 사용자에게 읽기 권한을 준다
 
 	if (fd==-1){
@@ -86,21 +75,11 @@ int fileDownload(char *type, char *path, char *ip, int sSock){
 	strcat(requestBuf,"192.168.198.141");
     
 	while(1){
-    int nRead = read(fd, requestBuf, sizeof(requestBuf));
-
-	if(nRead < 0){
-		perror("nRead");
-		return -1;
-	}
-	else if(nRead == 0){
-		break;
-	}
-    
-	while(1){
-	int nWritten=write(sSock, requestBuf, sizeof(requestBuf));
-	if (nWritten < 0){
-		perror("write");
-		return -1;
+		int nWritten=write(cSock, requestBuf, sizeof(requestBuf));
+		if (nWritten < 0){
+			perror("write");
+			return -1;
+		}
 	}
 
 	close(fd);
@@ -109,9 +88,9 @@ int fileDownload(char *type, char *path, char *ip, int sSock){
 	strcat(actBuf,"./");
 	strcat(actBuf,path);
 
-	int fd = open(actBuf, O_RDWR, O_CREAT, O_TRUNC, 0744);
+	int fd2 = open(actBuf, O_RDWR, O_CREAT, O_TRUNC, 0744);
     while(1){
-		int nRead = read(fd, actBuf,sizeof(actBuf));
+		int nRead = read(fd2, actBuf,sizeof(actBuf));
 
 		if(nRead < 0){
 			perror("nRead");
@@ -122,63 +101,49 @@ int fileDownload(char *type, char *path, char *ip, int sSock){
 		}
 	}
     
-	while(1){
-		int nWritten=write(sSock, actBuf, sizeof(actBuf));
+   /*	while(1){
+		int nWritten=write(cSock, actBuf, sizeof(actBuf));
         if (nWritten < 0){
 			perror("write");
 			return -1;
 		}
-	}
-    close(fd);
+	}*/
 
+    close(fd2);
 	return 0;
 }
 
-int main(int argc, char **argv){
+int clientQuit(char *type, char *path, char *ip, int cSock){
 
-	unsigned int fileSize;
-	char fileName[100];
-	char recvBuffer[BUFFER_SIZE];
-    
-	if(argc !=2){
-		fprintf(stderr, "./ls OR ./download [filePath]");
+	if (type == NULL || path == NULL || ip == NULL){
+		perror("quit");
 		return -1;
 	}
-	
+
+	int fd = open("./request.txt", O_RDWR, O_CREAT, O_TRUNC, 0744);
+
+	if (fd==-1){
+		perror("quit");
+		return -1;
+	}
+
+	char buf[BUFSIZ];
+	strcat(buf,"type:");
+	strcat(buf,"quit\n");
+	strcat(buf,"path:");
+	strcat(buf,"./\n");
+	strcat(buf,"ip:");
+	strcat(buf,"192.168.198.144");
+
 	while(1){
-			int nWritten=write(fd, buf, sizeof(buf));
-			if(nWritten<0){
-				perror("write");
-				return -1;
-			}//request.txt
+		int nWritten=write(cSock, buf, sizeof(buf));
+		if(nWritten<0){
+			perror("write");
+			close(fd);
+			return -1;
 		}
-		close(1);
-		dup(fd);
-
-		while(1){
-			//키보드로부터 데이터를 읽습니다.
-			int nRead = read(0, buf, sizeof(buf));
-			if (nRead < 0) {
-				perror("read");
-				return -1;
-			}
-			else if (nRead == 0) {
-				break;
-			}
-
-			int nWritten=write(sSock, buf, sizeof(buf));
-			if (nWritten < 0){
-				perror("write");
-				return -1;
-			}
-		} 
 	}
+	close(fd);
 
-    if (strcmp(type, "ls")==0){
-		listDownload();
-	}
-    else if (strcmp(type,"downLoad")==0){
-		fileDownload();
-	}
-	else if (strcmp(type, "download")==0){
-       }
+}
+
---
+++ sendRequest.h
@@ -0,0 +1,21 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include "FtpCommand.h"
+#include "ClientSocket.h"
+#include "listOpen.h"
+#define BUFSIZ 1024
+#define END_OF_PROTOCOL "\r\n"
+
+
+int fileDownload();
+int listDownload();
+int quit();
