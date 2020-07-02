8586ec87640125a3cc66de0640fb2649fc524a16
ahjheeju129 <heeju1112@naver.com>
2020-07-02 15:52:43 +0900

    client_temp writing

--- client_temp/client.c
+++ client_temp/client.c
@@ -1,63 +1,31 @@
- //Echo Client
-#include <sys/types.h>       
-#include <sys/socket.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <pthread.h>
-#include "clientStart.h"
-//#include "listOpen.h"
+#include <errno.h>
 
-void localThread(void * arg){
+void * localThread(void * arg){
 
 }
+int main(){
+	char * ip;
+	if(IP_INSERT_PAGE(&ip)==-1){
+		//클라이언트에게 ip재입력 받음
+	}
 
-int clientStart(const char * ip, int * sock){
-    //make socket for client
-    sock=socket(PF_INET,SOCK_STREAM,0);
-    if(sock==-1){
-        perror("socket");
-        return -1;
-    }
+	pthread_t tid;
+	if(pthread_create(&tid,NULL,localThread,NULL)==EAGAIN){
+		fprintf(stderr,"client_main:thread생성 실패\n");
+		return -1;
+	}
 
-    struct sockaddr_in addr = {0,};
-    addr.sin_family=AF_INET;
-    addr.sin_port=htons((int)PORT_NUM);
-    addr.sin_addr.s_addr=inet_addr((char * )ip);
+	int* ret;
+	if(pthread_join(tid,&ret)!=0){
+		fptintf(stderr,"client_main:thread join실패\n");
+		return -1;
+	}
 
-    //try to connect to server
-    if(connect(sock,(struct sockaddr*)&addr,sizeof(addr))==-1){
-        perror("connect");
-        return -1;
-    }
+	
 
-//    //get command from keyboard
-//    char buf[BUFSIZ];
-//    int nRead=read(0,buf,sizeof(buf)); //stdin
-//
-//    if(nRead<0){
-//        perror("read");
-//        return -1;
-//    }
-//    else if(nRead==0){
-//        return -1;
-//    }
-//
-//    //request to server
-//    buf[nRead]='\0';
-//    write(sock,buf,nRead);
-//
-//    nRead=read(sock,buf,sizeof(buf));
-//    if(nRead<0){
-//        perror("read");
-//        return -1;
-//    }
-//    else if(nRead==0){
-//        return -1;
-//    }
-//
-//    buf[nRead]='\0';
-//    fputs(buf,stdout);
-    return 0;
-}
+	
+
+
+
+
