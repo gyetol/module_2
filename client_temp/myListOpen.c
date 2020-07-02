2ce7b1c772b26557ae442a0b194121b48932944d
ahjheeju129 <heeju1112@naver.com>
2020-07-02 16:01:15 +0900

    폴더정리

--- client_temp/myListOpen.c
+++
@@ -1,6 +0,0 @@
-#include "myListOpen.h"
-
-int main(void){ //myListOpen();
-	system("/bin/ls");
-	return 0;
-}
--- client_temp/myListOpen.h
+++
@@ -1,7 +0,0 @@
-#ifndef MODULE_2_MYLISTOPEN_H
-#define MODULE_2_MYLISTOPEN_H
-
-#include<stdio.h>
-#include<stdlib.h>
-
-#endif
