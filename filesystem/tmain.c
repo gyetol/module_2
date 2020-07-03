0e42424f3710baf4d748c832c81cbbc1a447bd2b
gyetol <econostein@naver.com>
2020-07-03 15:46:56 +0900

    commit

--- filesystem/tmain.c
+++ filesystem/tmain.c
@@ -5,15 +5,16 @@
 
 int main(){
 	char *msg=".";
-	char* ary[]={"b.c","bb.c"};
-	char *paths = "fo";
+	char* ary[]={"b.c"};
+	//char *paths = "fo";
 	//doRemove(ary,sizeof(ary)/sizeof(ary[0]),&msg);
-	doCopy(ary,sizeof(ary)/sizeof(ary[0]),paths,&msg);	
+	//doCopy(ary,sizeof(ary)/sizeof(ary[0]),paths,&msg);	
 	//printf("%s\n",msg);
-	char *newPath = "folder";
-	doMkdir(newPath,&msg);
+	//char *newPath = "folder";
+	//doMkdir(newPath,&msg);
 	//printf("%s\n",msg);
-	doMove(ary,sizeof(ary)/sizeof(ary[0]),newPath,&msg);
+	char *newPath ="a.c";
+	doRename(ary,sizeof(ary)/sizeof(ary[0]),newPath,&msg);
 	printf("%s\n",msg);
 
 	return 0;
