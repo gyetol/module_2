b7292c0726f44e148dc3e1ffe6f671e116c19cba
gyetol <econostein@naver.com>
2020-07-05 14:09:59 +0900

    commit

--- filesystem/thread_main.c
+++ filesystem/thread_main.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/socket.h>
+
 
 void myflush(){
 	while(getchar()!='\n'){;}
 }
 
-void doCommand(){
+void doCommand(int sock, const char *ip){
 	char cmd;
 	char *srcPath[100];
 	char *destPath=NULL;
