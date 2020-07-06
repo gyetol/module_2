b202f8c314285cc02cd7e1685723162f0fc1ec11
gyetol <econostein@naver.com>
2020-07-06 22:01:57 +0900

    connect

---
+++ Client/commandFunc.h
@@ -0,0 +1,23 @@
+#ifndef MODULE_2_COMMANDFUNC_H
+#define MODULE_2_COMMANDFUNC_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include <fcntl.h>
+
+int putSrcPath(char **srcPath, char **msg);
+int freeDestPath(char *destPath, char **msg);
+int getDestPath(char *destPath, char **msg, const char * checkMsg);
+int doRemove(char **srcPath, int len, char **msg);
+int doMkdir(char *newPath, char **msg);
+int doMove(char **srcPath, int len, char *destPath, char **msg);
+int doCopy(char **srcPath, int len, char *destPath, char **msg);
+int doRename(char **srcPath, int len, char *destPath, char **msg);
+int putSrcPath(char **srcPath, char **msg);
+void myflush();
+
+#endif //MODULE_2_COMMANDFUNC_H
--- Client/doCommand.c
+++ Client/doCommand.c
@@ -1,49 +1,78 @@
 
 #include "doCommand.h"
-/*
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<pthread.h>
-#include "commandFunc.h"
-#include "sendRequest.h"
-#include "listOpen.h"
-#include "myListOpen.h"
 
-void * doListThread(ResInfo *resInfo){
-	listDownload(resInfo->sock,resInfo->ip);
-	return NULL;
+void * doListThread(void * arg){
+	if(arg==NULL){
+		fprintf(stderr, "doListThread: argument is null\n");
+		return NULL;
+	}
+	int * retVal=calloc(1,sizeof(int));
+	if(retVal==NULL){
+		perror("calloc");
+		return NULL;
+	}
+	ResInfo* resInfo =(ResInfo*)arg;
+	if(listDownload(resInfo->sock,resInfo->ip)==-1){
+		*retVal=-1;
+		return retVal;
+	}
+	*retVal=0;
+	return retVal;
 }
 
-void * doDownloadThread(ResInfo *resInfo){
+void * doDownloadThread(void *arg){
+	if(arg==NULL){
+		fprintf(stderr, "doDownloadThread: argument is null\n");
+		return NULL;
+	}
+	int * retVal=calloc(1,sizeof(int));
+	if(retVal==NULL){
+		perror("calloc");
+		return NULL;
+	}
+	ResInfo* resInfo =(ResInfo*)arg;
 	char fName[255];
 	printf("다운받을 파일명을 입력해주세요:");
 	fgets(fName,sizeof(fName),stdin);
-	fileDownload(resInfo->sock,resInfo->ip,&fName);
-	return NULL;
+
+	if(fileDownload(resInfo->sock,resInfo->ip,fName)==-1){
+		*retVal=-1;
+		return retVal;
+	}
+	*retVal=0;
+	return retVal;
 }
 
-void * doQuitThread(ResInfo *resInfo){
-	clientQuit(resInfo->sock, resInfo->ip);
-	return NULL;
+void * doQuitThread(void * arg){
+	if(arg==NULL){
+		fprintf(stderr, "doQuitThread: argument is null\n");
+		return NULL;
+	}
+	int * retVal=calloc(1,sizeof(int));
+	if(retVal==NULL){
+		perror("calloc");
+		return NULL;
+	}
+	ResInfo* resInfo =(ResInfo*)arg;
+	if(clientQuit(resInfo->sock, resInfo->ip)==-1){
+		*retVal=-1;
+		return retVal;
+	}
+
+	*retVal=0;
+	return retVal;
 }
 
-
-void myflush(){
-	while(getchar()!='\n'){;}
-}
 
 int  doCommand(ResInfo *resInfo){
 	char cmd;
-	char *srcPath[100];
-	char *destPath=NULL;
+	char *srcPath[100]={"b.c","bb.c"}; //하드코딩함
+	char *destPath="folder";
 	char *msg=".";
 	int flag = 0; // doCommand의 while문 계속: 0 , 탈출: 1
+	int len = sizeof(srcPath)/sizeof(srcPath[0]);
 	pthread_t tid;
+
 
 
 	while(1){
@@ -55,7 +84,7 @@ int  doCommand(ResInfo *resInfo){
 		case ' ' : putSrcPath(srcPath, &msg);
 				   break;
 
-		case '\n': if(pthread_create(&tid,NULL,doListThread, *resInfo)!=0){
+		case '\n': if(pthread_create(&tid,NULL,doListThread, resInfo)!=0){
 					   perror("pthread_create");
 					   return -1;
 				   } //서버쪽의 디렉토리 경로 요청시 list 쓰레드 생성
@@ -67,8 +96,8 @@ int  doCommand(ResInfo *resInfo){
 				   break;
 
 		case 'm' : getDestPath(destPath, &msg, "이동할 경로를 입력하세요:");
-				   doMove(srcPath,len,destpath,&msg);
-				   freeDestPath(destpath,&msg);
+				   doMove(srcPath,len,destPath,&msg);
+				   freeDestPath(destPath,&msg);
 				   break;
 
 		case 'r' : doRemove(srcPath,len,&msg);
@@ -79,7 +108,7 @@ int  doCommand(ResInfo *resInfo){
 				   freeDestPath(destPath,&msg);
 				   break;
 
-		case 'f' : if(pthread_create(&tid,NULL,doDownloadThread,*resInfo)!=0){
+		case 'f' : if(pthread_create(&tid,NULL,doDownloadThread,resInfo)!=0){
 					   perror("pthread_create");
 					   return -1;
 				   } //download 요청시 download 쓰레드 생성
@@ -92,7 +121,7 @@ int  doCommand(ResInfo *resInfo){
 				   freeDestPath(destPath,&msg);
 				   break;
 
-		case 'x' : if(pthread_create(&tid,NULL,doQuitThread, *resInfo)!=0){
+		case 'x' : if(pthread_create(&tid,NULL,doQuitThread, resInfo)!=0){
 					   perror("phread_create");
 					   return -1;
 				   } //quit 요청시 quit 쓰레드 생성
@@ -101,9 +130,8 @@ int  doCommand(ResInfo *resInfo){
 		}
 
 	}
+//}
 
-}
-*/
 
 /*
 void * fileDownloadThread(void * arg){
@@ -113,7 +141,7 @@ void * fileDownloadThread(void * arg){
 	return NULL;
 }
 */
-
+/*
 int doCommand(ResInfo* resInfo){
 	printf("(doCommand)doCommand 들어옴\n");
 	//listDownload(resInfo->sock,resInfo->ip);
@@ -122,7 +150,7 @@ int doCommand(ResInfo* resInfo){
 	//pthread_t tid;
 	//pthread_create(&tid,NULL,fileDownloadThread,resInfo);
 	//pthread_join(tid,NULL);
-	//printf("(doCommand)fileDownload성공\n");
+	//printf("(doCommand)fileDownload성공\n");*/
 	clientQuit(resInfo->sock,resInfo->ip);	printf("clientQuit성공\n");
 	return 0;
 }
